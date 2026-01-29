bc4f5b2edca4b6dbd6833cad4f870c8ac2b5f0fb
esraaehab333 <esraaehabb333@gmail.com>
2026-01-29 14:34:35 +0200

    lab 2

---
+++ MaterialDesignLab2/app/src/main/java/com/example/materialdesignlab2/Fragment3.java
@@ -0,0 +1,22 @@
+package com.example.materialdesignlab2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Fragment3#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Fragment3 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_3, container, false);
+    }
+}
